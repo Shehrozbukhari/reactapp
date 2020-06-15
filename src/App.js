51de427a2f2189d5aa722d269c13bc0b52391458
Shehrozbukhari <66364090+Shehrozbukhari@users.noreply.github.com>
2020-06-15 12:24:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './style.css';
 
 function App() {
   let [count, setCount]= useState(1);
-  let [timechange, setTimee]= useState(false);
+  let [timechangee, setTimee]= useState(false);
   return (
-    <div ClassName={`daytime ${timechange ? 'nighttime' : ''}`}>
+    <div ClassName={`daytime ${timechangee ? 'nighttime' : ''}`}>
         <h1>Num of comments {count}</h1>
         <button onClick= "{() => setCount(count + 1) }">Update counter</button>
-        <button onClick= "{() => setTimee(!timechange) }">Time Changer</button>
+        <button onClick= "{() => setTimee(!timechangee) }">Time Changer</button>
         
     </div>
     
