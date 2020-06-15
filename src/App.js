4637856d7ac09d46879f4841c71e24e4091beb6d
Shehrozbukhari <66364090+Shehrozbukhari@users.noreply.github.com>
2020-06-15 12:14:10 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div ClassName={` daytime {timechange ? 'nighttime' : ''} `}>
         <h1>Num of comments {count}</h1>
         <button onClick= "{() => setCount(count + 1) }">Update counter</button>
-        <button onClick= "{() => setTimee(!timechange) }"Time Changer</button>
+        <button onClick= "{() => setTimee(!timechange) }">Time Changer</button>
         
     </div>
     
