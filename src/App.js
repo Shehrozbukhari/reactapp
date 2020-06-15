558c2f5b0f33116e482f2847d378b4fe4da148f9
Shehrozbukhari <66364090+Shehrozbukhari@users.noreply.github.com>
2020-06-15 12:07:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 // import logo from './logo.svg';
-// import './App.css';
+import './style.css';
 
 function App() {
   let [count, setCount]= useState(1);
+  let [timechange, setTimee]= useState(True);
   return (
-    <div>
+    <div ClassName="{`daytime {timechange ? 'nighttime' : ''}`}">
         <h1>Num of comments {count}</h1>
         <button onClick= "{() => setCount(count + 1) }">Update counter</button>
+        <button onClick= "{() => setTimee(!timechange) }"Time Changer</button>
+        
     </div>
     
   );
