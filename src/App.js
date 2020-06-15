4d48e7e892afc3f98a614072410ac6f08c6505c7
Shehrozbukhari <66364090+Shehrozbukhari@users.noreply.github.com>
2020-06-15 11:53:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ function App() {
   return (
     <div>
         <h1>Num of comments {count}</h1>
-        <button onlcick={() => setCount(count + 1) }>Update counter</button>
+        <button onClick= {() => setCount(count + 1) }>Update counter</button>
     </div>
     
   );
 }
-
 export default App;
