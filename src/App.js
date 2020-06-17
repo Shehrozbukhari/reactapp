784c919489b34b650b0b9fdc7125dd313eb9e6d9
Shehrozbukhari <66364090+Shehrozbukhari@users.noreply.github.com>
2020-06-17 12:03:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   let [count, setCount]= useState(1);
   let [ismorning, setTimee]= useState(true);
   return (
-    <div className={ismorning ? 'nighttime' : ''}>
+    <div className="{ismorning ? 'nighttime' : 'daytime'}">
         <h1>Num of comments {count}</h1>
         <button onClick= "{() => setCount(count + 1) }">Update counterr</button>
         <button onClick= "{() => setTimee(!ismorning) }">Time Changer</button>
